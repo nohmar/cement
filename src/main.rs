da2f8627f69b8c0f79a4b4d21c49dd8b106ed3ee
Sung Noh <noh.sung@gmail.com>
2023-11-28 08:39:25 -0800

    Run application in `main.rs`
    Replace the contents of the entrypoint with:
    
    1. A new instance of `App`
    2. Connection initialization via `Connection` struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+pub mod models;
+pub mod schema;
+
+mod app;
+mod cli;
+mod connection;
+
+use app::App;
+use connection::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new(None);
+
+    let connection = Connection::new(None);
+    let connection_state = &mut connection.state.unwrap();
+
+    println!("{}", app.output_from_args(connection_state).unwrap());
 }
